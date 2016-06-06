687cd2ff9328a6ca1c6d56658d2d4273579b5486
Andrew Wong <andrew.wongsf@toogood.com>
2016-06-05 20:20:45 -0400

    added jquery and app.js to index.html

---
+++ js/app.js
@@ -0,0 +1,33 @@
+/*--(1--*/
+// Use the filters-example.html file to guide your decision making
+// Using progressive enhancement, your work should affect the index.html file.
+
+/*--(2)--*/
+// Only 10 students should be shown at a time
+// Calculate # of pages needed and add the appropriate number of links to the bottom of the page
+
+/*--(3)--*/
+// Hide all but first 10 students when page loads
+
+/*--(4)--*/
+// When user clicks "2", students 11-20 are shown. When user clicks
+// "3", students 21-30 are shown etc.
+
+/*--(5)--*/
+// Using progressive enhancement, add student search markup in filters-example.html to index.html
+
+/*--(6)--*/
+// Add an event listener to the search button
+	// When user clicks button it should use the text
+	// in the search input to filter the results.
+
+/*--(7)--*/
+// Users should be able to search by name or e-mail address 
+// Partial matches, like just a first name, should be displayed in the results.
+
+/*--(8)--*/
+// Search results should also be paginated
+// Eg. if the search returns more than 10 results, those results should be paginated too.
+
+/*--(9)--*/
+// Mke sure you can check off all of the items on the Student Project Submission Checklist. 
