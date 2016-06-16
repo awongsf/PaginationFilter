a83f998acd0bc7603ebb5672bca1db29c51f870b
Andrew Wong <andrew.wongsf@toogood.com>
2016-06-16 09:20:52 -0400

    added functionality to search button; refactored pagination

--- js/app.js
+++ js/app.js
@@ -20,46 +20,59 @@
 // Only 10 students should be shown at a time
 // Calculate # of pages needed and add the appropriate number of links to the bottom of the page
 var studentList = document.getElementsByClassName('student-item');
-var numberOfPages = Math.ceil(studentList.length / 10);
-var pagination = "<div class='pagination'><ul>";
 
-for (var p = 0; p < numberOfPages; p++)
-{
-	if (p === 0) {
-		pagination += "<li>\n<a class='active' href='#'>" + (p + 1) + "</a>\n</li>";
-	} else {
-		pagination += "<li>\n<a href='#'>" + (p + 1) + "</a>\n</li>";
+var createPagination = function (list) {
+	var numberOfPages = Math.ceil(list.length / 10);
+	var pagination = "<div class='pagination'><ul>";
+
+	for (var p = 0; p < numberOfPages; p++)
+	{
+		if (p === 0) {
+			pagination += "<li>\n<a class='active' href='#'>" + (p + 1) + "</a>\n</li>";
+		} else {
+			pagination += "<li>\n<a href='#'>" + (p + 1) + "</a>\n</li>";
+		}
 	}
+	pagination += "</ul></div>";
+	$(".page").append(pagination);
 }
-pagination += "</ul></div>";
-$(".page").append(pagination);
+
+createPagination(studentList);
 
 /*--(3)--*/
 // Hide all but first 10 students when page loads
-for (var i = 10; i < studentList.length; i++)
-{
-	$(studentList[i]).hide();
+var showFirstTenOnly = function (list) {
+	for (var i = 10; i < list.length; i++)
+	{
+		$(list[i]).hide();
+	}
 }
+
+showFirstTenOnly(studentList);
 
 /*--(4)--*/
 // When user clicks "2", students 11-20 are shown. When user clicks
 // "3", students 21-30 are shown etc.
-$(".pagination > ul > li > a").each(function () {
-	$(this).click(function () {
-		var activePage = document.getElementsByClassName("active");
-		$(activePage).removeClass("active");
-		$(this).addClass("active");
-		var pageNumber = $(this).parent().index();
-		getStudentList(pageNumber);
+var createPageLinks = function (list) {
+	$(".pagination > ul > li > a").each(function () {
+		$(this).click(function () {
+			var activePage = document.getElementsByClassName("active");
+			$(activePage).removeClass("active");
+			$(this).addClass("active");
+			var pageNumber = $(this).parent().index();
+			getStudentList(pageNumber, list);
+		});
 	});
-});
+};
 
-var getStudentList = function (page) {
+createPageLinks(studentList);
+
+var getStudentList = function (page, list) {
 	var startingPoint = page * 10;
-	$(studentList).hide();
+	$(list).hide();
 	for (var k = startingPoint; k < (startingPoint + 10); k++)
 	{
-		$(studentList[k]).show();
+		$(list[k]).show();
 	}
 };
 
@@ -72,10 +85,32 @@ $(".page-header").append('<div class="student-search">\n<input placeholder="Sear
 	// When user clicks button it should use the text
 	// in the search input to filter the results.
 var searchInput = document.getElementsByTagName("input")[0];
+var searchResults = []
 
 var printSearchResults = function () {
-	console.log(searchInput.value);
 	$(studentList).hide();
+	var searchFilter = new RegExp(searchInput.value);
+	for (var i = 0; i < studentList.length; i++)
+	{
+		var name = searchFilter.test($(studentList[i].getElementsByTagName("h3")).text());
+		var email = searchFilter.test($(studentList[i].getElementsByTagName("span")).text());
+		if (name === true || email === true)
+		{
+			$(studentList[i]).show();
+		}
+	}
+	searchResults = $("li.student-item:visible").toArray();
+	$(".pagination").remove();
+	if (searchResults.length > 10)
+	{
+		createPagination(searchResults);
+		showFirstTenOnly(searchResults);
+		createPageLinks(searchResults);
+	}
+	if (searchResults.length == 0)
+	{
+		$(".page").append("<h2>Sorry, no matches found.</h2>");
+	}
 };
 
 var searchButton = document.getElementsByTagName("button")[0];
@@ -90,4 +125,12 @@ searchButton.addEventListener("click", printSearchResults);
 // Eg. if the search returns more than 10 results, those results should be paginated too.
 
 /*--(9)--*/
-// Mke sure you can check off all of the items on the Student Project Submission Checklist. 
+// Make sure you can check off all of the items on the Student Project Submission Checklist. 
+
+
+// Include simple animations when transitioning between pages.
+
+// As the user types in the search box, dynamically filter the student listings. 
+// In other words, after each letter is typed into the search box, display any listings that match.
+
+// If no matches are found, include a message in the HTML to tell the user there are no matches.
